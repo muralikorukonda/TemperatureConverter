59ac84716c86980c52e3b38320484e620e0a2b43
Murali <Murali@Tejass-MacBook-Pro.local>
2015-04-16 13:57:10 +0800

    Added MyScript File

---
+++ MyScript.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  MyScript.sh
+#  TemperatureConverter
+#
+#  Created by Murali on 4/16/15.
+#  Copyright (c) 2015 Murali. All rights reserved.
+
+#!/bin/bash
+xcodebuild -configuration Debug -target "TemperatureConverterTests" build
